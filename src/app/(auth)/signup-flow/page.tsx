4218e6ab5f6854023b2605f9bf7b3b3c89d03067
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-05-23 14:17:44 +0300

    feat: on boarding setup page for new users

---
+++ src/app/(auth)/signup-flow/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import OnboardingForm from "./_components/OnboardingForm";
+import { ModeToggle } from "~/app/_components/toggle";
+
+const page = () => {
+  return (
+    <div className="relative flex h-[90vh] flex-col items-center">
+      <div className="flex w-full justify-end gap-2 p-4">
+        <ModeToggle />
+      </div>
+      <div className="my-auto box-border flex w-[90vw] max-w-[600px] flex-col items-center justify-center rounded-xl bg-muted p-4 shadow-lg">
+        <div className="flex flex-col items-center justify-center py-4">
+          <Image src={"/logo.png"} alt="" height={48} width={48} />
+          <h2 className="my-3 text-center text-xl font-bold">
+            Welcome to FitQuest
+          </h2>
+          <p>Let personalize your journey</p>
+        </div>
+        <OnboardingForm />
+      </div>
+    </div>
+  );
+};
+
+export default page;
