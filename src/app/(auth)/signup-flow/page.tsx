2dc7862200294161cc627d3bf564fa5bbf9805a6
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-05-25 00:56:30 +0300

    feat: added user popover with logout button

--- src/app/(auth)/signup-flow/page.tsx
+++ src/app/(auth)/signup-flow/page.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import React from "react";
 import OnboardingForm from "./_components/OnboardingForm";
-import { ModeToggle } from "~/app/_components/toggle";
+import OnboardingNav from "./_components/OnboardingNav";
 
 const page = () => {
   return (
     <div className="relative flex h-[90vh] flex-col items-center">
-      <div className="flex w-full justify-end gap-2 p-4">
-        <ModeToggle />
-      </div>
+      <OnboardingNav />
       <div className="my-auto box-border flex w-[90vw] max-w-[600px] flex-col items-center justify-center rounded-xl bg-muted p-4 shadow-lg">
         <div className="flex flex-col items-center justify-center py-4">
           <Image src={"/logo.png"} alt="" height={48} width={48} />
