15281a4b4a5c797812c70e3cec1b5733a99dfe3d
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-05-25 00:56:00 +0300

    fix: fully handled route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,56 @@
-import { NextResponse } from "next/server";
+import { NextFetchEvent, NextResponse } from "next/server";
 import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { getSession } from "next-auth/react";
 
-export default withAuth(
-  function middleware(request: NextRequestWithAuth) {
-    console.log(request.nextauth.token);
-    if (
-      request.nextUrl.pathname.startsWith("/dashboard") &&
-      request.nextauth.token?.isSetup === false
-    ) {
-      return NextResponse.redirect(new URL("/signup-flow", request.url));
-    }
-    if (
-      request.nextUrl.pathname.startsWith("/sign-in") &&
-      request.nextauth.token
-    ) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
+export default async function middleware(
+  request: NextRequestWithAuth,
+  event: NextFetchEvent,
+) {
+  const pathname = request.nextUrl.pathname;
+  const token = await getToken({ req: request });
+  const isAuthenticated = !!token;
+
+  if (pathname.startsWith("/sign-in") && isAuthenticated) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  const authMiddleware = withAuth(
+    async (req: NextRequestWithAuth) => {
+      const requestForNextAuth = {
+        headers: {
+          cookie: req.headers.get("cookie"),
+        },
+      };
+
+      // const session = await getSession({ req: requestForNextAuth as any });
+      const isSetup = req.nextauth?.token?.isSetup ?? false;
+
+      if (pathname.startsWith("/dashboard")) {
+        if (!isSetup) {
+          return NextResponse.redirect(new URL("/signup-flow", request.url));
+        }
+      }
+
+      if (pathname.startsWith("/signup-flow")) {
+        if (isSetup) {
+          return NextResponse.redirect(new URL("/dashboard", request.url));
+        }
+      }
     },
-  },
-);
+    {
+      pages: {
+        signIn: "/sign-in",
+      },
+      callbacks: {
+        authorized: ({ token }) => !!token,
+      },
+    },
+  );
+
+  return authMiddleware(request, event);
+}
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard", "/signup-flow", "/sign-in"],
 };
