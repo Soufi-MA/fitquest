eaffb0b1173e2528b0fb4b2323b51bbad0ce8926
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-05-28 18:11:10 +0300

    feat: get start of the week helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getStartOfWeek(today: Date) {
+  const currentDay = today.getDay();
+  const daysToSubtract = currentDay === 0 ? 6 : currentDay - 1;
+  const startDate = new Date(today.setDate(today.getDate() - daysToSubtract));
+  startDate.setHours(0, 0, 0, 0);
+
+  return startDate;
 }
