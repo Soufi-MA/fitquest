7adc67ca80392faa0c95efa5afb936cfc0b811f0
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-05-28 18:09:15 +0300

    feat: added meal logger component to meals page

--- src/app/dashboard/(tabs)/meals/page.tsx
+++ src/app/dashboard/(tabs)/meals/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import MealLogger from "./_components/MealLogger";
 
 const page = () => {
-  return <div>Meals</div>;
+  return (
+    <div className="px-2 py-2 md:px-8 md:py-4">
+      <div className="grid grid-cols-1 md:grid-cols-12">
+        <MealLogger />
+      </div>
+    </div>
+  );
 };
 
 export default page;
