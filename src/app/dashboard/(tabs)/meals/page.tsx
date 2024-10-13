e96435ba2585946441530368d640448f9e58b0a3
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-10-13 23:49:16 +0300

    fix: change horizontal padding

--- src/app/dashboard/(tabs)/meals/page.tsx
+++ src/app/dashboard/(tabs)/meals/page.tsx
@@ -3,7 +3,7 @@ import MealLogger from "./_components/MealLogger";
 
 const page = () => {
   return (
-    <div className="px-2 py-2 md:px-8 md:py-4">
+    <div className="px-2 py-2 md:px-4 md:py-4">
       <div className="grid grid-cols-1 md:grid-cols-12">
         <MealLogger />
       </div>
