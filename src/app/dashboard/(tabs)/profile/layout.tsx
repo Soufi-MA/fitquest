a0c8f67856f5e51ab44724b1aa2e5b2bfa22832c
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-10-19 22:19:44 +0300

    fix: style layout header

--- src/app/dashboard/(tabs)/profile/layout.tsx
+++ src/app/dashboard/(tabs)/profile/layout.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 import React, { type ReactNode } from "react";
 import ProfileNav from "./_components/ProfileNav";
 import { getCurrentUser } from "@/lib/session";
+import { Card } from "@/components/ui/card";
 
 const ProfileLayout = async ({ children }: { children: ReactNode }) => {
   const user = await getCurrentUser();
   return (
     <div className="flex flex-col gap-4 px-8 py-4">
       <div className="flex w-full flex-col justify-center">
-        <div className="flex w-full flex-col items-center gap-4 rounded-md bg-muted px-8 py-4 shadow-md md:flex-row">
+        <Card className="flex w-full flex-col items-center gap-4 rounded-md bg-card/60 backdrop-blur-md px-8 py-4 shadow-md md:flex-row">
           {user && (
             <>
               {user && user.profilePicture && (
@@ -36,7 +37,7 @@ const ProfileLayout = async ({ children }: { children: ReactNode }) => {
               </div>
             </>
           )}
-        </div>
+        </Card>
       </div>
       <ProfileNav />
       {children}
