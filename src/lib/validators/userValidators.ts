b0721e66b880e658423e9a559ee2223f457d7723
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-10-19 22:22:33 +0300

    feat: update user profile schema validator

--- src/lib/validators/userValidators.ts
+++ src/lib/validators/userValidators.ts
@@ -22,3 +22,20 @@ export const AccountDetailsValidator = z.object({
 });
 
 export type TAccountDetailsValidator = z.infer<typeof AccountDetailsValidator>;
+
+export const updateUserValidator = z.object({
+  name: z.string().min(1, "Name is required"),
+  gender: z.enum(["Male", "Female"]),
+  height: z
+    .number()
+    .min(100, "Minimum height exceeded")
+    .max(300, "Maximum height exceeded"),
+  heightUnit: z.enum(["cm", "in"]),
+  weight: z
+    .number()
+    .min(30, "Minimum weight exceeded")
+    .max(300, "Maximum weight exceeded"),
+  weightUnit: z.enum(["kg", "lb"]),
+  birthDay: z.date(),
+});
+export type TUpdateUserValidator = z.infer<typeof updateUserValidator>;
