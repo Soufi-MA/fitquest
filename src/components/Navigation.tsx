5d1f65ef439a0b5da4090a4272822fe75ddc757a
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-10-20 14:56:36 +0300

    fix: hide conic background gradient on light mode

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 const Navigation = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex min-h-screen flex-col pb-20 md:flex-row-reverse md:pb-0">
-      <span className="absolute bg-[conic-gradient(var(--tw-gradient-stops))] from-primary to-background top-[calc(100vh/4)] w-[60%] h-[calc(100vh/2)] blur-[100px] -z-10 opacity-30 dark:opacity-25 left-0"></span>
+      <span className="hidden dark:block absolute bg-[conic-gradient(var(--tw-gradient-stops))] from-primary to-background top-[calc(100vh/4)] w-[60%] h-[calc(100vh/2)] blur-[100px] -z-10 opacity-30 dark:opacity-25 left-0"></span>
       <div className="flex flex-grow flex-col">{children}</div>
       <Sidebar />
     </div>
