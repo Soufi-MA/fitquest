aa771a95a37455fae5f18b74eb65bfa713553a70
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-10-26 13:04:59 +0300

    fix: wrap UserNav in a client wrapper to use usePathname in navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,14 @@
 import React, { type ReactNode } from "react";
 import Navigation from "../../components/Navigation";
 import Navbar from "../../components/Navbar";
+import UserNav from "@/components/UserNav";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
     <Navigation>
-      <Navbar />
+      <Navbar>
+        <UserNav />
+      </Navbar>
       {children}
     </Navigation>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,21 @@
-// "use client";
+"use client";
 
-import React from "react";
+import React, { ReactNode } from "react";
 
-// import { usePathname } from "next/navigation";
-// import { DASHBOARD_TABS, PROFILE_TABS } from "../config";
-import UserNav from "./UserNav";
+import { usePathname } from "next/navigation";
+import { DASHBOARD_TABS, PROFILE_TABS } from "../config";
 
-const Navbar = () => {
-  // const pathname = usePathname();
+const Navbar = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
 
   return (
     <div className="sticky top-0 z-10 flex h-16 items-center justify-between border-b bg-background px-4 md:h-20 md:px-8">
       <p className="text-base font-semibold text-foreground md:text-3xl">
-        {/* {[...DASHBOARD_TABS, ...PROFILE_TABS].find(
+        {[...DASHBOARD_TABS, ...PROFILE_TABS].find(
           (tab) => tab.href === pathname
-        )?.label ?? ""} */}
+        )?.label ?? ""}
       </p>
-      <UserNav />
+      {children}
     </div>
   );
 };
