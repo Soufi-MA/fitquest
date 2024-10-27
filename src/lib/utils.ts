7ec509e9c26d6329cdfc350ed505bdd4abe21e78
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-10-27 19:49:40 +0300

    fix: get start of the week without modifying the input date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,8 +9,9 @@ export function cn(...inputs: ClassValue[]) {
 export function getStartOfWeek(today: Date) {
   const currentDay = today.getDay();
   const daysToSubtract = currentDay === 0 ? 6 : currentDay - 1;
-  const startDate = new Date(today.setDate(today.getDate() - daysToSubtract));
-  startDate.setHours(0, 0, 0, 0);
+  const startDate = new Date(today);
+  startDate.setDate(today.getDate() - daysToSubtract);
+  startDate.setHours(6, 0, 0, 0);
 
   return startDate;
 }
