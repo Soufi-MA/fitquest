6c05ce3353b15f88f2ef8e742480dffa3593caf5
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-10-29 22:48:20 +0300

    fix: refactor fetching location

--- src/app/dashboard/(tabs)/meals/page.tsx
+++ src/app/dashboard/(tabs)/meals/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import MealLogger from "./_components/MealLogger";
 
-const page = () => {
+type PageProps = {
+  searchParams: Promise<{
+    date?: string;
+  }>;
+};
+
+const page = async ({ searchParams }: PageProps) => {
+  const { date } = await searchParams;
+
   return (
     <div className="px-2 py-2 md:px-4 md:py-4">
       <div className="grid grid-cols-1 md:grid-cols-12">
-        <MealLogger />
+        <MealLogger date={date} />
       </div>
     </div>
   );
