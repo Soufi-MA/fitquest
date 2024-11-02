b859458087b10b0c63152b27f02d811d9c83b6ed
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-11-02 18:55:50 +0300

    feat: full meal and food entries server actions

--- src/app/dashboard/(tabs)/meals/_components/EditMealElements/actions.ts
+++ src/app/dashboard/(tabs)/meals/_components/EditMealElements/actions.ts
@@ -3,9 +3,12 @@
 import { db } from "@/db/connection";
 import { mealFoodTable, mealTable } from "@/db/schema/food";
 import { getCurrentUser } from "@/lib/session";
-import { and, eq } from "drizzle-orm";
+import { and, eq, ExtractTablesWithRelations } from "drizzle-orm";
+import { NodePgQueryResultHKT } from "drizzle-orm/node-postgres";
+import { PgTransaction } from "drizzle-orm/pg-core";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
+import { fetchFood } from "../../actions";
 
 const AddFoodSchema = z.object({
   mealId: z.string(),
@@ -197,6 +200,154 @@ export const deleteMeal = async (formData: FormData) => {
     .returning({ mealId: mealTable.id });
 
   if (deleted.mealId) {
+    revalidatePath("/dashboard/meals");
+    return { success: true };
+  }
+
+  return { success: false };
+};
+
+const getNewTotalCalories = async (
+  mealId: string,
+  tx: PgTransaction<
+    NodePgQueryResultHKT,
+    Record<string, never>,
+    ExtractTablesWithRelations<Record<string, never>>
+  >
+) => {
+  let totalCalories = 0;
+  let totalProtein = 0;
+  let totalCarb = 0;
+  let totalFats = 0;
+
+  const mealFoods = await tx
+    .select()
+    .from(mealFoodTable)
+    .where(eq(mealFoodTable.mealId, mealId));
+
+  await Promise.all(
+    mealFoods.map(async (foodItem) => {
+      const addedNutrients = new Set<string>();
+      const foodData = await fetchFood(foodItem.foodId);
+
+      foodData.nutrients.forEach((nutrient) => {
+        const { name, amount } = nutrient;
+
+        if (!addedNutrients.has(name.toLowerCase())) {
+          const totalAmount =
+            amount * ((foodItem.servingSize / 100) * foodItem.quantity);
+
+          if (name.toLowerCase().startsWith("energy")) {
+            totalCalories += totalAmount;
+          } else if (name.toLowerCase().startsWith("protein")) {
+            totalProtein += totalAmount;
+          } else if (name.toLowerCase().startsWith("carbohydrate")) {
+            totalCarb += totalAmount;
+          } else if (name.toLowerCase().startsWith("total lipid (fat)")) {
+            totalFats += totalAmount;
+          }
+
+          addedNutrients.add(name.toLowerCase());
+        }
+      });
+    })
+  );
+
+  return {
+    totalCalories,
+    totalProtein,
+    totalCarb,
+    totalFats,
+  };
+};
+
+export const editFoodEntry = async (formData: FormData) => {
+  const user = await getCurrentUser();
+  if (!user) return { unauthorized: true };
+
+  const quantity = formData.get("quantity") as string;
+  const portionId = formData.get("portionId") as string;
+  const servingSize = formData.get("servingSize") as string;
+  const id = formData.get("id") as string;
+
+  let updated;
+
+  await db.transaction(async (tx) => {
+    const [updatedFoodEntry] = await tx
+      .update(mealFoodTable)
+      .set({
+        portionId: portionId.length ? portionId : undefined,
+        quantity: Number(quantity),
+        servingSize: Number(servingSize),
+      })
+      .where(eq(mealFoodTable.id, id))
+      .returning({
+        foodEntryId: mealFoodTable.id,
+        mealId: mealFoodTable.mealId,
+      });
+
+    const newTotals = await getNewTotalCalories(updatedFoodEntry.mealId, tx);
+
+    const [updatedMeal] = await tx
+      .update(mealTable)
+      .set({
+        totalCalories: newTotals.totalCalories,
+        totalCarbs: newTotals.totalCarb,
+        totalFats: newTotals.totalFats,
+        totalProtein: newTotals.totalProtein,
+      })
+      .where(eq(mealTable.id, updatedFoodEntry.mealId))
+      .returning({ mealId: mealTable.id });
+
+    if (updatedFoodEntry.foodEntryId && updatedMeal.mealId) {
+      updated = true;
+    }
+  });
+
+  if (updated === true) {
+    revalidatePath("/dashboard/meals");
+    return { success: true };
+  }
+
+  return { success: false };
+};
+
+export const deleteFoodEntry = async (formData: FormData) => {
+  const user = await getCurrentUser();
+  if (!user) return { unauthorized: true };
+
+  const foodEntryId = formData.get("id") as string;
+
+  let deleted;
+  await db.transaction(async (tx) => {
+    const [deletedFoodEntry] = await tx
+      .delete(mealFoodTable)
+      .where(eq(mealFoodTable.id, foodEntryId))
+      .returning({
+        foodEntryId: mealFoodTable.id,
+        mealId: mealFoodTable.mealId,
+      });
+
+    const newTotals = await getNewTotalCalories(deletedFoodEntry.mealId, tx);
+
+    const [updatedMeal] = await tx
+      .update(mealTable)
+      .set({
+        totalCalories: newTotals.totalCalories,
+        totalCarbs: newTotals.totalCarb,
+        totalFats: newTotals.totalFats,
+        totalProtein: newTotals.totalProtein,
+      })
+      .where(eq(mealTable.id, deletedFoodEntry.mealId))
+      .returning({ mealId: mealTable.id });
+
+    if (deletedFoodEntry.foodEntryId && updatedMeal.mealId) {
+      deleted = true;
+    }
+  });
+
+  if (deleted === true) {
+    revalidatePath("/dashboard/meals");
     return { success: true };
   }
 
