cffe32f59e01d4b311b78109988010b261317027
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-11-03 14:17:43 +0300

    feat: fetch user goal

--- src/app/dashboard/(tabs)/meals/_components/MealLogger.tsx
+++ src/app/dashboard/(tabs)/meals/_components/MealLogger.tsx
@@ -5,19 +5,24 @@ import MealLoggerHeader from "./MealLoggerHeader";
 import MealLoggerSummary from "./MealLoggerSummary";
 import MealLoggerDetails from "./MealLoggerDetails";
 import { getDateFromSearchParams } from "@/lib/utils";
-import { fetchMealDetails } from "../actions";
+import { fetchMealDetails, fetchUserGoal } from "../actions";
 
 const MealLogger = async ({ date }: { date: string | undefined }) => {
   const today = getDateFromSearchParams(date);
   const getChchedMealDetails = cache(fetchMealDetails);
   const mealDetailsPromise = getChchedMealDetails(today);
+  const getCachedUserGoal = cache(fetchUserGoal);
+  const userGoalPromise = getCachedUserGoal();
 
   return (
     <div className="col-span-full flex flex-col rounded-md bg-muted/60 backdrop-blur-md border">
       <MealLoggerHeader />
       <Separator />
-      <div className="grid grid-cols-1 lg:grid-cols-2 w-full flex-col lg:flex-row sm:h-[412px] overflow-hidden peer-data-[pending=true]:animate-pulse">
-        <MealLoggerSummary mealDetailsPromise={mealDetailsPromise} />
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-full flex-col lg:flex-row lg:h-[412px] overflow-hidden peer-data-[pending=true]:animate-pulse">
+        <MealLoggerSummary
+          mealDetailsPromise={mealDetailsPromise}
+          userGoalPromise={userGoalPromise}
+        />
         <MealLoggerDetails mealDetailsPromise={mealDetailsPromise} />
       </div>
     </div>
