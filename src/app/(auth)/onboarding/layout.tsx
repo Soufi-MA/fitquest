7a2001bae39d766b420306e70712802b3821f525
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-11-05 20:06:56 +0300

    feat: add logout button on the onboarding layout

--- src/app/(auth)/onboarding/layout.tsx
+++ src/app/(auth)/onboarding/layout.tsx
@@ -1,5 +1,7 @@
-import { getUserOnboardingStatus } from "@/app/actions";
+import { getUserOnboardingStatus, logout } from "@/app/actions";
 import { ModeToggle } from "@/components/toggle";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 import { redirect } from "next/navigation";
 import { type ReactNode } from "react";
 
@@ -9,8 +11,18 @@ const OnboardingLayout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="relative flex h-[90vh] flex-col items-center">
-      <div className="flex justify-end w-full py-4 px-2">
+      <div className="flex justify-center md:justify-end items-center w-full py-4 px-2 z-20">
         <ModeToggle />
+        <form action={logout} className="flex flex-col items-center gap-2 px-2">
+          <Button
+            type="submit"
+            variant={"ghost"}
+            className="flex h-10 cursor-pointer items-center gap-2 rounded-md px-4"
+          >
+            <LogOut size={20} />
+            <p>Log Out</p>
+          </Button>
+        </form>
       </div>
       {children}
     </div>
