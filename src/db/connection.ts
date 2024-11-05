aae51f1e52f195c4d17f4c554353fd395b9dfa23
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2024-11-06 00:29:41 +0300

    fix: update db url

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,6 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString:
-    "postgres://postgres:014262911@localhost:5432/fitquest?sslmode=no-verify",
+  connectionString: process.env.POSTGRES_URL!,
 });
 export const db = drizzle(pool);
