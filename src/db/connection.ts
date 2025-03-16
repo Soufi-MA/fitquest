7a78b6c2d0101653f7e0d484a77bf60765390764
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2025-03-16 22:31:53 +0300

    import dotenv for seeding

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,5 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL!,
